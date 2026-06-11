4a1fcfe0746cb896954bf36f71964811bb1baa1e
JonathanPortela <jonathan.portela97@gmail.com>
2026-06-11 20:02:52 -0300

    muchas cosas

--- tarea/src/Main.java
+++ tarea/src/Main.java
@@ -4,47 +4,55 @@ import java.util.Scanner;
 public class Main {
 
 
-
     public static void main(String[] args) {
-        int opcionElegida = opciones();
-        switch (opcionElegida) {
-
-            case 1:
-            mostrarSaludo();
-            case 2:
-            saludar();
-            case 3:
-            System.out.println("¡Ahora calculemos el area de un triangulo!");
-
+        int opcionWhile = 879876876;
+        System.out.println("Para finalizar el programa pongale 0");
+        while (opcionWhile != 0) {
             Scanner teclado = new Scanner(System.in);
+            System.out.println("Que vamos a hacer?");
+            int opcionElegida = opciones();
+            opcionWhile = opcionElegida;
+            switch (opcionElegida) {
 
-            System.out.print("Ingresá su altura: ");
-            int altura = teclado.nextInt();
-            System.out.print("Ingresá su base: ");
-            int base = teclado.nextInt();
+                case 1:
+                    mostrarSaludo();
+                case 2:
+                    saludar();
+                case 3:
+                    System.out.println("¡Ahora calculemos el area de un triangulo!");
 
-            System.out.println("El area de su rectangulo es: " + calcularAreaTriangulo(base, altura));
-            case 4:
-            mayusculas();
-            case 5:
-                esPar();
-            break;
 
+                    System.out.print("Ingresá su altura: ");
+                    int altura = teclado.nextInt();
+                    System.out.print("Ingresá su base: ");
+                    int base = teclado.nextInt();
+
+                    System.out.println("El area de su rectangulo es: " + calcularAreaTriangulo(base, altura));
+                case 4:
+                    mayusculas();
+                case 5:
+                    esPar();
+                case 6:
+                    cuadradoDeUnNumero();
+                case 7:
+                    factorial();
+                case 8:
+                    sumaDeEnteros();
+                    break;
+
+            }
         }
     }
 
     //Metodos
 //1. Mostrar mensaje fijo
-    public static void mostrarSaludo()
-    {
+    public static void mostrarSaludo() {
         System.out.println("¡Hola mundo!");
     }
 //2. Saludo personalizado
 
 
-
-    public static void saludar()
-    {
+    public static void saludar() {
         Scanner teclado = new Scanner(System.in);
 
         System.out.print("Ingresá tu nombre: ");
@@ -53,14 +61,14 @@ public class Main {
         System.out.println("¡Hola " + nombreIngresado + "!!!");
 
     }
+
     //3. Área de un rectángulo
-    public static double calcularAreaTriangulo(double base, double altura)
-    {
-    return (base * altura);
+    public static double calcularAreaTriangulo(double base, double altura) {
+        return (base * altura);
     }
+
     //4. Convertir a mayúsculas
-    public static void mayusculas()
-    {
+    public static void mayusculas() {
         Scanner teclado = new Scanner(System.in);
 
         System.out.print("Ingresá tu oracion: ");
@@ -70,18 +78,21 @@ public class Main {
         System.out.println(oracion.toUpperCase());
 
     }
+
     //5. Mostrar menú
     public static int opciones() {
-        System.out.print("Elija una opcion: \n1-Saludar\n2-Saludarte\n3-Calcular area de rectangulo\n4-convertir todo a minuscula\n5-Es Par??\nESCRIBE UNA!!!::");
+        System.out.print("Elija una opcion: \n1-Saludar\n2-Saludarte\n3-Calcular area de rectangulo\n4-convertir todo a minuscula\n5-Es Par??\n6-Para saber el cuadrado de un numero\n7-Si desea calcular el factorial de un numero\n8-Suma de numeros indiscriminada.\nESCRIBE UNA!!!::");
         Scanner teclado = new Scanner(System.in);
 
 
         int opcion = teclado.nextInt();
         return opcion;
     }
+
     //6. Número par o impar
     public static int esPar() {
         int numero;
+        System.out.println("Ingrese un numero para ves si es par");
         Scanner teclado = new Scanner(System.in);
         numero = teclado.nextInt();
         int par = numero % 2;
@@ -90,7 +101,53 @@ public class Main {
         } else {
             System.out.println("No es par");
         }
-        
+
         return numero;
     }
+
+    //7. Calcular el cuadrado de un número
+    public static double cuadradoDeUnNumero() {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Ingresá su numero para saber su cuadrado: ");
+        double base = teclado.nextDouble();
+        double resultado = base * base;
+        System.out.println("El cuadrato de "+ base + " Es: "+ resultado );
+        return resultado;
+    }
+    //8. Factorial recursivo
+    public static int factorial() {
+        Scanner teclado = new Scanner(System.in);
+        int resultado = 0;
+
+
+        System.out.print("Ingresá su numero para ver el factorial: ");
+        int factorial = teclado.nextInt();
+
+        for (int i = 1;i <= factorial; i++) {
+            resultado += i * (factorial-1);
+        }
+        System.out.println(" Es: "+ resultado );
+
+        return resultado;
+    }
+    //9. Sumar múltiples números (varargs)
+    public static int sumaDeEnteros()
+    {
+        Scanner teclado = new Scanner(System.in);
+        int resultado = 0;
+        System.out.println("Si quere para de suma metele una letra.");
+        System.out.println("Ingrese el numero que desea sumar: ");
+
+        while (teclado.hasNextInt()){
+            System.out.println("Ingrese el numero que desea sumar: ");
+            int numeroIngresado = teclado.nextInt();
+            resultado += numeroIngresado;
+            System.out.println("Vas sumando: " + resultado);
+
+        }
+        System.out.println("Sumaste un total de " + resultado);
+            return resultado;
+    }
+
 }
