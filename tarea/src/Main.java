27487df4d0e141f90f1c133ed187d20caa39bf77
JonathanPortela <jonathan.portela97@gmail.com>
2026-06-11 23:03:15 -0300

    casi

--- tarea/src/Main.java
+++ tarea/src/Main.java
@@ -1,4 +1,5 @@
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -16,29 +17,43 @@ public class Main {
 
                 case 1:
                     mostrarSaludo();
+                    break;
                 case 2:
                     saludar();
+                    break;
                 case 3:
                     System.out.println("¡Ahora calculemos el area de un triangulo!");
-
-
                     System.out.print("Ingresá su altura: ");
                     int altura = teclado.nextInt();
                     System.out.print("Ingresá su base: ");
                     int base = teclado.nextInt();
-
                     System.out.println("El area de su rectangulo es: " + calcularAreaTriangulo(base, altura));
+                    break;
                 case 4:
                     mayusculas();
+                    break;
                 case 5:
                     esPar();
+                    break;
                 case 6:
                     cuadradoDeUnNumero();
+                    break;
                 case 7:
                     factorial();
+                    break;
                 case 8:
                     sumaDeEnteros();
                     break;
+                case 9:
+                    promedioArray();
+                    break;
+                case 10:
+                    numeroBuscar();
+                    break;
+                case 11:
+                    formaFormateada();
+                    break;
+
 
             }
         }
@@ -81,7 +96,9 @@ public class Main {
 
     //5. Mostrar menú
     public static int opciones() {
-        System.out.print("Elija una opcion: \n1-Saludar\n2-Saludarte\n3-Calcular area de rectangulo\n4-convertir todo a minuscula\n5-Es Par??\n6-Para saber el cuadrado de un numero\n7-Si desea calcular el factorial de un numero\n8-Suma de numeros indiscriminada.\nESCRIBE UNA!!!::");
+        System.out.print("Elija una opcion: \n1-Saludar\n2-Saludarte\n3-Calcular area de rectangulo\n4-convertir todo a minuscula\n5-Es Par??\n" +
+                "6-Para saber el cuadrado de un numero\n7-Si desea calcular el factorial de un numero\n8-Suma de numeros indiscriminada.\n" +
+                "-9 para sumar un promedio de numeros\n10-Juego de buscar en el array\n-11 aca te muestro un array...\nESCRIBE UNA!!!::");
         Scanner teclado = new Scanner(System.in);
 
 
@@ -118,7 +135,7 @@ public class Main {
     //8. Factorial recursivo
     public static int factorial() {
         Scanner teclado = new Scanner(System.in);
-        int resultado = 0;
+        int resultado = 1;
 
 
         System.out.print("Ingresá su numero para ver el factorial: ");
@@ -147,7 +164,148 @@ public class Main {
 
         }
         System.out.println("Sumaste un total de " + resultado);
-            return resultado;
+        return resultado;
     }
 
-}
+
+    //10. Promedio de un array
+    public static int promedioArray()
+    {
+        Scanner teclado = new Scanner(System.in);
+        double sumaTotal = 0;
+        System.out.println("Indique la cantidad de numeros que va a ingresar: ");
+        int largoArray = teclado.nextInt();
+
+        double[] numeros = new double[largoArray];
+        System.out.println("ingrese el numero: ");
+        for(int i = 0; i < numeros.length; i++)
+        {
+
+            numeros[i] = teclado.nextDouble();
+            System.out.println("otro mas: ");
+        }
+
+        for(int i = 0; i < numeros.length; i++)
+        {
+            sumaTotal += numeros[i];
+
+
+
+        }
+        System.out.println("El promedio es: " + sumaTotal/numeros.length);
+        return (int) sumaTotal;
+    }
+    //11. Buscar valor en array
+    public static int numeroBuscar()
+    {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Juego de buscar.... tu tarea es colocar un numero. si esta entre los 5 seleccionados ganaste!!!");
+
+        Random aleatorio = new Random();
+
+        int[] numeros = new int[5];
+        System.out.println("ingrese el numero: ");
+        int eleccion = teclado.nextInt();
+        for(int i = 0; i < numeros.length; i++)
+        {
+            int numeroAleatorio = aleatorio.nextInt(100);
+            numeros[i] = numeroAleatorio;
+        }
+        int lePego = 0;
+        for(int i = 0; i < numeros.length; i++)
+        {
+
+            if(numeros[i] == eleccion)
+            {
+                lePego = 1;
+            }
+
+        }
+        if(lePego == 1) {
+            System.out.println("----------------MUY BIEN TU NUMERO ESTABA ENTRE LOS 5 SELECCIONADOS!!!!----------------");
+        }
+        else {
+            System.out.println("--------------vas a tener que mejorar--------------------");
+
+        }
+        return eleccion;
+    }
+    //12. Contar ocurrencias
+    //Crear un método que reciba un array de String y un string clave, y devuelva cuántas veces aparece.
+    // NO ENTENDI QUE PIDE
+    //13. Imprimir array de forma formateada
+    public static int formaFormateada()
+    {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Recorremos el array");
+
+        Random aleatorio = new Random();
+
+        int[] numeros = new int[5];
+        for(int i = 0; i < numeros.length; i++)
+        {
+            int numeroAleatorio = aleatorio.nextInt(100);
+            numeros[i] = numeroAleatorio;
+        }
+        System.out.print("[");
+        for(int i = 0; i < numeros.length; i++)
+        {
+            System.out.print(numeros[i]);
+            if (i < numeros.length - 1)
+            {
+                System.out.print(",");
+
+            }
+        }
+        System.out.println("]");
+
+        int condador = 0;
+        condador = numeros.length;
+        System.out.println("y alrevez.");
+        System.out.print("[");
+
+        for(int i = 0; i < numeros.length; i++)
+        {
+            condador--;
+            System.out.print(numeros[condador]);
+            if (i < numeros.length - 1)
+            {
+                System.out.print(",");
+
+            }
+        }
+        System.out.println("]");
+        return 0;
+    }
+    //15. Estudiante con mejor nota
+    public static int estudiante()
+    {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("tu tarea es colocar la nota y el estudiante a quien corresponda!!!");
+
+        int[] notas = new int[5];
+        String[] nombres = new String[5];
+        int mayor = 0;
+        String estudiante = 0;
+        int eleccion = teclado.nextInt();
+        for(int i = 0; i < notas.length; i++)
+        {
+            System.out.println("ingrese la nota del estudiante");
+
+            notas[i] = teclado.nextInt();
+            System.out.println("Ingrese el nombre del estudiante");
+
+            nombres[i] = teclado.nextLine();
+        }
+        for(int i = 0; i < notas.length; i++)
+        {
+            if (notas[i] > mayor) {
+                mayor = notas[i];
+                estudiante = nombres[i];
+        }
+            System.out.println("El estudiante con mayor nota es" + estudiante + " con una nota de " + mayor);
+        }
+
+
+        return mayor;
+    }
