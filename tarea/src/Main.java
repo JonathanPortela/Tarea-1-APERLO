f0a91ec0a4863cd6eced54427058df918340274c
JonathanPortela <jonathan.portela97@gmail.com>
2026-06-12 11:02:31 -0300

    termine

--- tarea/src/Main.java
+++ tarea/src/Main.java
@@ -53,6 +53,9 @@ public class Main {
                 case 11:
                     formaFormateada();
                     break;
+                case 12:
+                    estudiante();
+                    break;
 
 
             }
@@ -98,7 +101,7 @@ public class Main {
     public static int opciones() {
         System.out.print("Elija una opcion: \n1-Saludar\n2-Saludarte\n3-Calcular area de rectangulo\n4-convertir todo a minuscula\n5-Es Par??\n" +
                 "6-Para saber el cuadrado de un numero\n7-Si desea calcular el factorial de un numero\n8-Suma de numeros indiscriminada.\n" +
-                "-9 para sumar un promedio de numeros\n10-Juego de buscar en el array\n-11 aca te muestro un array...\nESCRIBE UNA!!!::");
+                "-9 para sumar un promedio de numeros\n10-Juego de buscar en el array\n-11 aca te muestro un array...\n-12 vamos a ver cual de los estudiantes vale la pena\nESCRIBE UNA!!!::");
         Scanner teclado = new Scanner(System.in);
 
 
@@ -129,9 +132,10 @@ public class Main {
         System.out.print("Ingresá su numero para saber su cuadrado: ");
         double base = teclado.nextDouble();
         double resultado = base * base;
-        System.out.println("El cuadrato de "+ base + " Es: "+ resultado );
+        System.out.println("El cuadrato de " + base + " Es: " + resultado);
         return resultado;
     }
+
     //8. Factorial recursivo
     public static int factorial() {
         Scanner teclado = new Scanner(System.in);
@@ -141,22 +145,22 @@ public class Main {
         System.out.print("Ingresá su numero para ver el factorial: ");
         int factorial = teclado.nextInt();
 
-        for (int i = 1;i <= factorial; i++) {
-            resultado += i * (factorial-1);
+        for (int i = 1; i <= factorial; i++) {
+            resultado += i * (factorial - 1);
         }
-        System.out.println(" Es: "+ resultado );
+        System.out.println(" Es: " + resultado);
 
         return resultado;
     }
+
     //9. Sumar múltiples números (varargs)
-    public static int sumaDeEnteros()
-    {
+    public static int sumaDeEnteros() {
         Scanner teclado = new Scanner(System.in);
         int resultado = 0;
         System.out.println("Si quere para de suma metele una letra.");
         System.out.println("Ingrese el numero que desea sumar: ");
 
-        while (teclado.hasNextInt()){
+        while (teclado.hasNextInt()) {
             System.out.println("Ingrese el numero que desea sumar: ");
             int numeroIngresado = teclado.nextInt();
             resultado += numeroIngresado;
@@ -169,8 +173,7 @@ public class Main {
 
 
     //10. Promedio de un array
-    public static int promedioArray()
-    {
+    public static int promedioArray() {
         Scanner teclado = new Scanner(System.in);
         double sumaTotal = 0;
         System.out.println("Indique la cantidad de numeros que va a ingresar: ");
@@ -178,26 +181,23 @@ public class Main {
 
         double[] numeros = new double[largoArray];
         System.out.println("ingrese el numero: ");
-        for(int i = 0; i < numeros.length; i++)
-        {
+        for (int i = 0; i < numeros.length; i++) {
 
             numeros[i] = teclado.nextDouble();
             System.out.println("otro mas: ");
         }
 
-        for(int i = 0; i < numeros.length; i++)
-        {
+        for (int i = 0; i < numeros.length; i++) {
             sumaTotal += numeros[i];
 
 
-
         }
-        System.out.println("El promedio es: " + sumaTotal/numeros.length);
+        System.out.println("El promedio es: " + sumaTotal / numeros.length);
         return (int) sumaTotal;
     }
+
     //11. Buscar valor en array
-    public static int numeroBuscar()
-    {
+    public static int numeroBuscar() {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Juego de buscar.... tu tarea es colocar un numero. si esta entre los 5 seleccionados ganaste!!!");
 
@@ -206,53 +206,46 @@ public class Main {
         int[] numeros = new int[5];
         System.out.println("ingrese el numero: ");
         int eleccion = teclado.nextInt();
-        for(int i = 0; i < numeros.length; i++)
-        {
+        for (int i = 0; i < numeros.length; i++) {
             int numeroAleatorio = aleatorio.nextInt(100);
             numeros[i] = numeroAleatorio;
         }
         int lePego = 0;
-        for(int i = 0; i < numeros.length; i++)
-        {
+        for (int i = 0; i < numeros.length; i++) {
 
-            if(numeros[i] == eleccion)
-            {
+            if (numeros[i] == eleccion) {
                 lePego = 1;
             }
 
         }
-        if(lePego == 1) {
+        if (lePego == 1) {
             System.out.println("----------------MUY BIEN TU NUMERO ESTABA ENTRE LOS 5 SELECCIONADOS!!!!----------------");
-        }
-        else {
+        } else {
             System.out.println("--------------vas a tener que mejorar--------------------");
 
         }
         return eleccion;
     }
+
     //12. Contar ocurrencias
     //Crear un método que reciba un array de String y un string clave, y devuelva cuántas veces aparece.
     // NO ENTENDI QUE PIDE
     //13. Imprimir array de forma formateada
-    public static int formaFormateada()
-    {
+    public static int formaFormateada() {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Recorremos el array");
 
         Random aleatorio = new Random();
 
         int[] numeros = new int[5];
-        for(int i = 0; i < numeros.length; i++)
-        {
+        for (int i = 0; i < numeros.length; i++) {
             int numeroAleatorio = aleatorio.nextInt(100);
             numeros[i] = numeroAleatorio;
         }
         System.out.print("[");
-        for(int i = 0; i < numeros.length; i++)
-        {
+        for (int i = 0; i < numeros.length; i++) {
             System.out.print(numeros[i]);
-            if (i < numeros.length - 1)
-            {
+            if (i < numeros.length - 1) {
                 System.out.print(",");
 
             }
@@ -264,12 +257,10 @@ public class Main {
         System.out.println("y alrevez.");
         System.out.print("[");
 
-        for(int i = 0; i < numeros.length; i++)
-        {
+        for (int i = 0; i < numeros.length; i++) {
             condador--;
             System.out.print(numeros[condador]);
-            if (i < numeros.length - 1)
-            {
+            if (i < numeros.length - 1) {
                 System.out.print(",");
 
             }
@@ -277,35 +268,33 @@ public class Main {
         System.out.println("]");
         return 0;
     }
+
     //15. Estudiante con mejor nota
-    public static int estudiante()
-    {
+    public static int estudiante() {
         Scanner teclado = new Scanner(System.in);
         System.out.println("tu tarea es colocar la nota y el estudiante a quien corresponda!!!");
 
         int[] notas = new int[5];
         String[] nombres = new String[5];
         int mayor = 0;
-        String estudiante = 0;
-        int eleccion = teclado.nextInt();
-        for(int i = 0; i < notas.length; i++)
-        {
+        String estudiante = "0";
+        for (int i = 0; i < notas.length; i++) {
             System.out.println("ingrese la nota del estudiante");
 
             notas[i] = teclado.nextInt();
             System.out.println("Ingrese el nombre del estudiante");
-
+            teclado.nextLine();
             nombres[i] = teclado.nextLine();
         }
-        for(int i = 0; i < notas.length; i++)
-        {
+        for (int i = 0; i < notas.length; i++) {
             if (notas[i] > mayor) {
                 mayor = notas[i];
                 estudiante = nombres[i];
-        }
-            System.out.println("El estudiante con mayor nota es" + estudiante + " con una nota de " + mayor);
-        }
+            }
 
+        }
+        System.out.println("El estudiante con mayor nota es " + estudiante + " con una nota de " + mayor);
 
         return mayor;
     }
+}
